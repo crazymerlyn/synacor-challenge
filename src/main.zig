4127a8aedc58c20361eccd458c81305780022bf0
Ankit Goel <ankitgoel616@gmail.com>
2022-12-20 06:41:50 +0000

    Solved for the second code

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,68 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn getVal(x: u16, registers: [8]u16) !u16 {
+    return if (x < 32768) x else if (x < 32776) registers[x - 32768] else error.InvalidArgs;
+}
 
+pub fn runBinary(memory: []u16, stdout: anytype) !void {
+    var pc: u16 = 0;
+    var registers: [8]u16 = undefined;
+
+    while (true) {
+        // std.debug.print("Running opcode: {any}\n", .{memory[pc]});
+        switch (memory[pc]) {
+            0 => {
+                return;
+            },
+            19 => {
+                const a = try getVal(memory[pc + 1], registers);
+                if (a > 255) return error.InvalidArgs;
+                try stdout.print("{c}", .{@intCast(u8, a)});
+                pc += 2;
+            },
+            21 => {
+                pc += 1;
+            },
+            else => {
+                std.debug.print("Unknown opcode: {any}\n", .{memory[pc]});
+                return error.UnImplementedError;
+            },
+        }
+    }
+}
+
+pub fn main() !void {
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const stdout = std.io.getStdOut().writer();
+    var args = try std.process.argsWithAllocator(std.heap.page_allocator);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // skip my own exe name
+    _ = args.skip();
 
-    try bw.flush(); // don't forget to flush!
+    const filename = args.next() orelse {
+        std.debug.print("Expected first argument to be path to the binary\n", .{});
+        return error.InvalidArgs;
+    };
+
+    var buffer: [65536]u8 = undefined;
+
+    const bytes_read = try std.fs.cwd().readFile(filename, &buffer);
+
+    var memory = std.mem.zeroes([32768]u16);
+
+    for (bytes_read) |byte, i| {
+        memory[i / 2] += if (i % 2 == 0) byte else @intCast(u16, byte) * 256;
+    }
+
+    // var i: u16 = 0;
+    // while (i * 2 < bytes_read.len) {
+    //     std.debug.print("{any} ", .{memory[i]});
+    //     i += 1;
+    // }
+
+    try runBinary(&memory, stdout);
 }
 
 test "simple test" {
