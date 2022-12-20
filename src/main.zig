b4ed6758f4b41e3b50fa3f66ff16f002c8f8e9ee
Ankit Goel <ankitgoel616@gmail.com>
2022-12-20 07:30:19 +0000

    Passed self check tests

--- src/main.zig
+++ src/main.zig
@@ -1,41 +1,152 @@
 const std = @import("std");
 
-fn getVal(x: u16, registers: [8]u16) !u16 {
-    return if (x < 32768) x else if (x < 32776) registers[x - 32768] else error.InvalidArgs;
-}
+const Machine = struct {
+    memory: []u16,
+    registers: [8]u16,
+    stack: std.ArrayList(u16),
 
-pub fn runBinary(memory: []u16, stdout: anytype) !void {
-    var pc: u16 = 0;
-    var registers: [8]u16 = undefined;
+    fn getVal(self: *Machine, x: u16) !u16 {
+        return if (x < 32768) x else if (x < 32776) self.registers[x - 32768] else error.InvalidArgs;
+    }
 
-    while (true) {
-        // std.debug.print("Running opcode: {any}\n", .{memory[pc]});
-        switch (memory[pc]) {
-            0 => {
-                return;
-            },
-            19 => {
-                const a = try getVal(memory[pc + 1], registers);
-                if (a > 255) return error.InvalidArgs;
-                try stdout.print("{c}", .{@intCast(u8, a)});
-                pc += 2;
-            },
-            21 => {
-                pc += 1;
-            },
-            else => {
-                std.debug.print("Unknown opcode: {any}\n", .{memory[pc]});
-                return error.UnImplementedError;
-            },
+    fn setVal(self: *Machine, x: u16, val: u16) !void {
+        if ((x < 32768) or (x >= 32776)) return error.InvalidArgs;
+        self.registers[x - 32768] = val;
+    }
+
+    pub fn run(self: *Machine, stdin: anytype, stdout: anytype) !void {
+        var pc: u16 = 0;
+
+        while (true) {
+            // std.debug.print("Running opcode: {any}\n", .{self.memory[pc]});
+            switch (self.memory[pc]) {
+                0 => {
+                    return;
+                },
+                1 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    try self.setVal(self.memory[pc + 1], b);
+                    pc += 3;
+                },
+                2 => {
+                    const a = try self.getVal(self.memory[pc + 1]);
+                    try self.stack.append(a);
+                    pc += 2;
+                },
+                3 => {
+                    const a = self.stack.pop();
+                    try self.setVal(self.memory[pc + 1], a);
+                    pc += 2;
+                },
+                4 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    const c = try self.getVal(self.memory[pc + 3]);
+                    try self.setVal(self.memory[pc + 1], if (b == c) 1 else 0);
+                    pc += 4;
+                },
+                5 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    const c = try self.getVal(self.memory[pc + 3]);
+                    try self.setVal(self.memory[pc + 1], if (b > c) 1 else 0);
+                    pc += 4;
+                },
+                6 => {
+                    pc = try self.getVal(self.memory[pc + 1]);
+                },
+                7 => {
+                    const a = try self.getVal(self.memory[pc + 1]);
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    pc = if (a != 0) b else pc + 3;
+                },
+                8 => {
+                    const a = try self.getVal(self.memory[pc + 1]);
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    pc = if (a == 0) b else pc + 3;
+                },
+                9 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    const c = try self.getVal(self.memory[pc + 3]);
+                    try self.setVal(self.memory[pc + 1], (b + c) % 32768);
+                    pc += 4;
+                },
+                10 => {
+                    const b: u32 = try self.getVal(self.memory[pc + 2]);
+                    const c: u32 = try self.getVal(self.memory[pc + 3]);
+                    try self.setVal(self.memory[pc + 1], @intCast(u16, (b * c) % 32768));
+                    pc += 4;
+                },
+                11 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    const c = try self.getVal(self.memory[pc + 3]);
+                    try self.setVal(self.memory[pc + 1], b % c);
+                    pc += 4;
+                },
+                12 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    const c = try self.getVal(self.memory[pc + 3]);
+                    try self.setVal(self.memory[pc + 1], b & c);
+                    pc += 4;
+                },
+                13 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    const c = try self.getVal(self.memory[pc + 3]);
+                    try self.setVal(self.memory[pc + 1], b | c);
+                    pc += 4;
+                },
+                14 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    try self.setVal(self.memory[pc + 1], ~b & 0x7fff);
+                    pc += 3;
+                },
+                15 => {
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    try self.setVal(self.memory[pc + 1], self.memory[b]);
+                    pc += 3;
+                },
+                16 => {
+                    const a = try self.getVal(self.memory[pc + 1]);
+                    const b = try self.getVal(self.memory[pc + 2]);
+                    self.memory[a] = b;
+                    pc += 3;
+                },
+                17 => {
+                    const a = try self.getVal(self.memory[pc + 1]);
+                    try self.stack.append(pc + 2);
+                    pc = a;
+                },
+                18 => {
+                    const a = self.stack.popOrNull();
+                    pc = a orelse return;
+                },
+                19 => {
+                    const a = try self.getVal(self.memory[pc + 1]);
+                    if (a > 255) return error.InvalidArgs;
+                    try stdout.print("{c}", .{@intCast(u8, a)});
+                    pc += 2;
+                },
+                20 => {
+                    var x: u16 = try stdin.readByte();
+                    try self.setVal(self.memory[pc + 1], x);
+                    pc += 2;
+                },
+                21 => {
+                    pc += 1;
+                },
+                else => {
+                    std.debug.print("Unknown opcode: {any}\n", .{self.memory[pc]});
+                    return error.UnImplementedError;
+                },
+            }
         }
     }
-}
+};
 
 pub fn main() !void {
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
     const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
     var args = try std.process.argsWithAllocator(std.heap.page_allocator);
 
     // skip my own exe name
@@ -62,7 +173,11 @@ pub fn main() !void {
     //     i += 1;
     // }
 
-    try runBinary(&memory, stdout);
+    var stack = std.ArrayList(u16).init(std.heap.page_allocator);
+    defer stack.deinit();
+
+    var machine = Machine{ .memory = &memory, .registers = std.mem.zeroes([8]u16), .stack = stack };
+    try machine.run(stdin, stdout);
 }
 
 test "simple test" {
